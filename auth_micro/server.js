52cd691b653fa1c157fe60c1d58fd3a5e68c4fb7
Arman-Kabir <armankabir8@gmail.com>
2024-02-18 17:00:17 +0600

    register proto done server middleware added

--- auth_micro/server.js
+++ auth_micro/server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get('/', (req, res) => {
     return res.json({ message: "It's working" })
