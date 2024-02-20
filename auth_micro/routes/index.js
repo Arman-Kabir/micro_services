aa7c3f0792bb5f4339659df03ed5ddfe9fcf6a77
Arman-Kabir <armankabir8@gmail.com>
2024-02-20 17:19:03 +0600

    userRoute,getUsers done

--- auth_micro/routes/index.js
+++ auth_micro/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./AuthRoutes.js";
+import userRoutes from "./UserRoutes.js";
 
 const router = Router();
 
 router.use("/api",authRoutes);
+router.use("/api",userRoutes);
 
 
 export default router;
