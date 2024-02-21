9f40cd3ec683d494cf749357280ca5b387202fb2
Arman-Kabir <armankabir8@gmail.com>
2024-02-21 17:29:21 +0600

    PostController store done

--- post_micro/server.js
+++ post_micro/server.js
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
 });
 
 // Routes
-// import Routes from "./routes/index.js";
-// app.use(Routes);
+import Routes from "./routes/index.js";
+app.use(Routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
